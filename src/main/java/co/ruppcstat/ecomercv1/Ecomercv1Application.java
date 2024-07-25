0fc89062aad02f499716447660f11d05fa0e29ed
seansunly <sunlyslysun@gmail.com>
2024-07-26 01:02:04 +0700

    first commit

---
+++ src/main/java/co/ruppcstat/ecomercv1/Ecomercv1Application.java
@@ -0,0 +1,13 @@
+package co.ruppcstat.ecomercv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecomercv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ecomercv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/ruppcstat/ecomercv1/Ecomercv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ruppcstat.ecomercv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecomercv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
